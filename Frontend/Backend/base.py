15190f0143660f7a400ced7cf7e85bee819de8a8
bodino <a.bifulco23@gmail.com>
2022-11-19 12:28:05 -0800

    added wallet support

--- Frontend/Backend/base.py
+++ Frontend/Backend/base.py
@@ -1,12 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
 
-api = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)
