8ff7d7c82ae3f4186d1b67b2dc8543fb532cf11b
bodino <a.bifulco23@gmail.com>
2022-11-19 17:52:00 -0800

    data

--- Frontend/Backend/base.py
+++ Frontend/Backend/base.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import numpy as np
 import getFeatures
-import tensorflow as tf
-from tensorflow import keras 
+import pandas as pd
+# import tensorflow as tf
+# from tensorflow import keras 
 # instance of flask application
 app = Flask(__name__)
  
@@ -11,10 +13,15 @@ app = Flask(__name__)
 def hello_world(address):
     
     features = getFeatures.get_features_from_address(address)
-    print(features)
+    # print(features)
+    food = np.array(features)
+    df = pd.DataFrame(food)
+    print(df.shape)
+
+    # model = tf.keras.models.load_model('model')
+
     return str(features)
 
-    model = tf.keras.models.load_model('model')
  
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
