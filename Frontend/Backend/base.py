7ca5988c012e85303010a03709862daf6615618a
bodino <a.bifulco23@gmail.com>
2022-11-19 16:20:42 -0800

    model

--- Frontend/Backend/base.py
+++ Frontend/Backend/base.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import getFeatures
+import tensorflow as tf
+from tensorflow import keras 
 # instance of flask application
 app = Flask(__name__)
  
@@ -7,9 +9,12 @@ app = Flask(__name__)
 # when root url is accessed
 @app.route("/<address>")
 def hello_world(address):
+    
     features = getFeatures.get_features_from_address(address)
     print(features)
     return str(features)
+
+    model = tf.keras.models.load_model('model')
  
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
