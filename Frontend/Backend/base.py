ff380b6f8f763a3ce66ea650082cee005d5df189
bodino <a.bifulco23@gmail.com>
2022-11-19 20:06:32 -0800

    idk

--- Frontend/Backend/base.py
+++ Frontend/Backend/base.py
@@ -1,9 +1,8 @@
 from flask import Flask
 import numpy as np
 import getFeatures
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np 
+# import tensorflow as tf
+# from tensorflow import keras
 import pandas as pd
 
 # instance of flask application
@@ -13,24 +12,27 @@ app = Flask(__name__)
 # when root url is accessed
 @app.route("/<address>")
 def hello_world(address):
-    max_scaler = np.array(pd.read_csv('max.csv'))
-    min_scaler = np.array(pd.read_csv('min.csv'))
-    features = getFeatures.get_features_from_address(address)
-    new_features = np.array(features[1:])
-    final = []
-    for i in range(0,len(max_scaler)):
-        max = max_scaler[i]
-        min = min_scaler[i]
-        print('wtf')
-        print(max)
-        diff = max - min
-        final.append((new_features[i] - min)/diff)
-    df = pd.DataFrame(np.array(final))
-    print(df.shape)
+    data = (pd.read_csv('labeleddata - eth_illicit_features (1).csv'))
+    del data['flag']
+    print(data)
 
-    model = tf.keras.models.load_model('my_h5_model.h5')
-    result = model.predict(df.T)
-    return str(result)
+    # min_scaler = np.array(pd.read_csv('min.csv'))
+    # features = getFeatures.get_features_from_address(address)
+    # new_features = np.array(features[1:])
+    # final = []
+    # for i in range(0,len(max_scaler)):
+    #     max = max_scaler[i]
+    #     min = min_scaler[i]
+    #     print('wtf')
+    #     print(max)
+    #     diff = max - min
+    #     final.append((new_features[i] - min)/diff)
+    # df = pd.DataFrame(np.array(final))
+    # print(df.shape)
+
+    # model = tf.keras.models.load_model('my_h5_model.h5')
+    # result = model.predict(df.T)
+    return str("hi")
  
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
