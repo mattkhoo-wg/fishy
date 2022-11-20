40285e2fb1369e305cea73b31ae7df8b577d0aa4
Matthew Khoo <mattkhoo1234@gmail.com>
2022-11-19 20:06:07 -0800

    backend finished

--- Frontend/Backend/base.py
+++ Frontend/Backend/base.py
@@ -5,31 +5,32 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np 
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # instance of flask application
 app = Flask(__name__)
  
 # home route that returns below text
 # when root url is accessed
+def get_transform_model():
+    df = pd.read_csv('labeleddata - eth_illicit_features (1).csv')
+    X = df.drop(['flag'], axis=1)
+    norm = MinMaxScaler().fit(X)
+    return norm
+
 @app.route("/<address>")
 def hello_world(address):
-    max_scaler = np.array(pd.read_csv('max.csv'))
-    min_scaler = np.array(pd.read_csv('min.csv'))
     features = getFeatures.get_features_from_address(address)
     new_features = np.array(features[1:])
-    final = []
-    for i in range(0,len(max_scaler)):
-        max = max_scaler[i]
-        min = min_scaler[i]
-        print('wtf')
-        print(max)
-        diff = max - min
-        final.append((new_features[i] - min)/diff)
-    df = pd.DataFrame(np.array(final))
-    print(df.shape)
+    new_features = new_features.astype(np.float)
+    norm = get_transform_model()
+    df_final = pd.DataFrame(new_features)
+    X = norm.transform(df_final.T)
+    print('look at me!')
+    #print(df_final.shape)
 
     model = tf.keras.models.load_model('my_h5_model.h5')
-    result = model.predict(df.T)
+    result = model.predict(X)
     return str(result)
  
 if __name__ == '__main__':
